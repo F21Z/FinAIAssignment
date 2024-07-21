74bd91ee3d08e91feae89fc602fdc16fa29a4b81
Faizuddin Mohammed <115614604+F21Z@users.noreply.github.com>
2024-07-21 13:59:07 +0530

    ticker_list.py

---
+++ FinxAI/trading_pipeline/ticker_list.py
@@ -0,0 +1,61 @@
+# Source :: https://www.cnbc.com/dow-30/
+
+DOW30_TICKERS = [
+    "AAPL",
+    "AMGN",
+    "AMZN",
+    "AXP",
+    "BA",
+    "CAT",
+    "CRM",
+    "CSCO",
+    "CVX",
+    "DIS",
+    "GOOG",
+    "GS",
+    "HD",
+    "HON",
+    "IBM",
+    "INTC",
+    "JNJ",
+    "JPM",
+    "KO",
+    "MMM",
+    "MRK",
+    "MSFT",
+    "NKE",
+    "NVDA",
+    "PG",
+    "QCOM",
+    "TRV",
+    "UNH",
+    "V",
+    "VZ",
+    "WMT"
+    ]
+
+# CUSTOM_TICKERS = [
+#     "AMZN", #1997
+#     "AAPL", #1984
+#     "GOOG", #2014
+#     "QCOM", #1991
+#     "NVDA", #1999
+#     "MSFT", #1986
+#     "VRTX", #1991
+#     "NVO"   #1984
+#     "WMT",  #1984
+#     "ZS",   #2018
+#     "ETSY", #2015
+#     "NTNX", #2016
+#     "BIIB", #1991
+#     "PLTR", #2020
+#     "META", #2012
+#     "CVX",  #1984
+#     "ALSN", #2012
+#     "NBIX", #1996
+#     "ABNB", #2020
+#     "UBER", #2019
+#     "CMG",  #2006
+#     "CRWD", #2019
+# ]
+
